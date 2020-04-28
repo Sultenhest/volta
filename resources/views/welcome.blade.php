3505d40869dd36543e363e79d77271cf0e39dbc7
Simon Konstantyner <simon@konstantyner.dk>
2020-04-28 18:39:28 +0200

    removed links from welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,6 +48,7 @@
                 font-size: 84px;
             }
 
+            .subtitle,
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -84,15 +85,8 @@
                     {{ config('app.name', 'Laravel') }}
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="subtitle">
+                    <p>Nothing to see here..</p>
                 </div>
             </div>
         </div>
