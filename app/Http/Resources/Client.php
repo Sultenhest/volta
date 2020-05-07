5fdfbef9ba6060487821bc9b9fb2b40d11ae4c25
Simon Konstantyner <simon@konstantyner.dk>
2020-05-07 15:22:58 +0200

    concat arrays

--- app/Http/Resources/Client.php
+++ app/Http/Resources/Client.php
@@ -14,9 +14,7 @@ class Client extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
+        return parent::toArray($request) + [
             'description' => $this->description,
             'vat_abbr' => $this->vat_abbr,
             'vat' => $this->vat,
